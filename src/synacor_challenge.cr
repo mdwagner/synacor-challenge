1a3eeeb0d4b5c2bd8f3c5e4c4c6107740d2e7246
Michael Wagner <mdwranger@gmail.com>
2021-12-13 00:05:08 -0500

    feat: wip

--- src/synacor_challenge.cr
+++ src/synacor_challenge.cr
@@ -1,6 +1,52 @@
-# TODO: Write documentation for `SynacorChallenge`
-module SynacorChallenge
-  VERSION = "0.1.0"
+struct UInt16
+  MAX_U15 = (2 ** 15).to_u16
 
-  # TODO: Put your code here
+  def add_u15(other : UInt16)
+    ((self + other) % MAX_U15).to_u16
+  end
+
+  def mul_u15(other : UInt16)
+    ((self * other) % MAX_U15).to_u16
+  end
+end
+
+module SynacorChallenge
+  class VM
+    # file: challenge.bin in read-only binary mode
+    @file : File
+
+    # TODO: memory with 15-bit address space storing 16-bit values
+    @memory = Array(UInt16).new(2 ** 15)
+
+    # TODO: eight registers
+    @registers = uninitialized UInt16[8]
+
+    # TODO: unbounded stack which holds individual 16-bit values
+    @stack = Array(UInt16).new
+
+    # binary format
+    # each number is stored as a 16-bit little-endian pair (low byte, high byte)
+    # numbers 0..32767 mean a literal value
+    # numbers 32768..32775 instead mean registers 0..7
+    # numbers 32776..65535 are invalid
+    # programs are loaded into memory starting at address 0
+    # address 0 is the first 16-bit value, address 1 is the second 16-bit value, etc
+
+    def initialize(@file)
+    end
+
+    def main
+      slice = Bytes.new(2)
+      while @file.read_fully?(slice)
+        @memory << IO::ByteFormat::LittleEndian.decode(UInt16, slice)
+      end
+
+      pp! @memory[0..10]
+      pp! @memory.size
+    end
+  end
+end
+
+File.open("#{__DIR__}/../instructions/challenge.bin", mode: "rb") do |f|
+  SynacorChallenge::VM.new(f).main
 end
